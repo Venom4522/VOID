690d10e4c90d7e9f962ad1a208f36ba2e891d6b3
Venom4522 <devkumar4193@gmail.com>
2026-03-16 00:27:25 +0530

    Adding Lipstics and Eyeliner Portfolio Item

--- Production/script.js
+++ Production/script.js
@@ -18,6 +18,9 @@ const projects = [
   { title: 'Spy Camera Cinematic Showcase', cat: 'Product Animation', year: '2026', type: 'video', videoId: 'wE6YlvS5PoI', thumbnail: 'video_thumbnail.png' },
   { title: 'Charger - Cinematic Showcase', cat: 'Product Animation', year: '2025', type: 'video', videoId: '5L7uCbVqzo8', thumbnail: 'thumb_short1.png', aspect: 'vertical' },
   { title: 'Phone Holder - Showcase', cat: '3D Animation', year: '2025', type: 'video', videoId: 'uYYYT2geang', thumbnail: 'thumb_short2.png', aspect: 'vertical' }
+  { title: 'Lipstics Reveal', cat: '3D Animation', year: '2025', type: 'video', videoId: 'W1IoGSSVosU', thumbnail: 'thumb_short4.png', aspect: 'vertical' }
+  { title: 'Eyeliner Reveal', cat: '3D Animation', year: '2025', type: 'video', videoId: 'P5nUwMpS-F8', thumbnail: 'thumb_short3.png', aspect: 'vertical' }
+
 ];
 
 const grid = document.getElementById('portfolioGrid');
